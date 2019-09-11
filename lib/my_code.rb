004776692521956c4e51298ebcb5712397437d01
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 03:35:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ end
 def reduce_to_total(source_array, starting_point = 0)
   total = starting_point
   counter = 0 
-  while counter < array.length do
+  while counter < array.size do
     total += array[counter]
   counter += 1
   end
