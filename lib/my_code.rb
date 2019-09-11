035ad25826c18119a41bd6c75b3b7ae6bc3017e1
Sebastian Delima <sebastiandelimagec@gmail.com>
2019-09-11 03:26:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,53 @@
-# My Code here....
+
+def map_to_negativize(source_array)
+  i = 0 
+  answers = []
+  while i < source_array.length do
+    a = source_array[i] * -1
+    i += 1
+    answers.push(a)
+  end
+  return answers
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  i = 0 
+  answers = []
+  while i < source_array.length do
+    a = source_array[i] * 2
+    i += 1
+    answers.push(a)
+  end
+  return answers
+end
+
+def map_to_square(source_array)
+   i = 0 
+  answers = []
+  while i < source_array.length do
+    a = source_array[i] ** 2
+    i += 1
+    answers.push(a)
+  end
+  return answers
+end
+
+def reduce_to_total(source_array, starting_point)
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
